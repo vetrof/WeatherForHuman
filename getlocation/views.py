7ec3e5d238d767dbfc640716fe7039f967d63ac0
Vitaly Vetrof <111348000+vetrof@users.noreply.github.com>
2023-04-20 19:05:22 +0600

    readme

--- getlocation/views.py
+++ getlocation/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def get_location(request):
-
     loc = request.GET.get('location')
     print('***************')
     print(loc)
-
 
     return render(request, 'getlocation/get_location.html')
