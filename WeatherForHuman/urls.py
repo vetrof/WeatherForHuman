6581a46b1f4009935403b9203aa45c58feea2370
Vitaly Vetrof <vetrof@gmail.com>
2023-07-02 02:37:08 +0600

    new_desighn

--- WeatherForHuman/urls.py
+++ WeatherForHuman/urls.py
@@ -9,7 +9,7 @@ from feel_data.views import save_feel_data
 from getlocation.views import get_location
 from getweather.views import get_current_weather, get_forecast_weather, \
     weather_api
-from index_view.views import index_view
+from index_view.views import index_view, NewIndex
 from login.views import LoginUserView
 from my_feel_history.views import FeelHistoryView
 from registration.views import UserRegistrationView
@@ -21,16 +21,15 @@ urlpatterns = [
     path('registration/', UserRegistrationView.as_view(), name='registration'),
 
     path('', index_view, name='index'),
+    path('new/', NewIndex.as_view(), name='new_index'),
     path('get_location/', get_location, name='get_location'),
     path('check_location/', check_location, name='check_location'),
 
     path('get_weather/<str:loc>', get_current_weather, name='get_weather'),
-    path('get_forecast/1/<str:loc>', get_forecast_weather,
-         name='get_forecast_1'),
+    path('get_forecast/1/<str:loc>', get_forecast_weather, name='get_forecast_1'),
 
     path('save_feel_data/', save_feel_data, name='save_feel_data'),
-    path('feel_history/', login_required(FeelHistoryView.as_view()),
-         name='feel_history'),
+    path('feel_history/', login_required(FeelHistoryView.as_view()), name='feel_history'),
 
     # api
     path('api/<str:loc>', weather_api),
--- index_view/views.py
+++ index_view/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def index_view(request):
-
     return render(request, 'index.html')
+
+
+class NewIndex(TemplateView):
+    template_name = 'new_index.html'
