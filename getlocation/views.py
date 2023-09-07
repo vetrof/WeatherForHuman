8db54348aee522e29e69934fb848ff99d8f7f231
Vitaly Vetrof <vetrof@gmail.com>
2023-09-08 01:30:17 +0600

    add geolocation
    add 2 way geolocation: geo ip and ask browser you coordinates

--- getlocation/views.py
+++ getlocation/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+import requests
+from WeatherForHuman.settings import IPINFO_KEY, OPEN_CA_DATA_KEY
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 def get_location(request):
-    loc = request.GET.get('location')
-    print('***************')
-    print(loc)
+    ip = get_client_ip(request)
 
-    return render(request, 'getlocation/get_location.html')
+    # Отправляем запрос к API для определения города по IP
+    api_key = 'b4c1625c91da62'
+    response = requests.get(f'https://ipinfo.io/{ip}?token={IPINFO_KEY}')
+    data = response.json()
+
+    city = data.get('city', 'Unknown')  # Получаем название города или "Unknown", если не удалось определить
+
+    return render(request, 'getlocation/get_location.html', {'ip': ip, 'city': city, 'OPEN_CA_DATA_KEY': OPEN_CA_DATA_KEY})
